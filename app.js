fa2f96df6d8cf1f546ccc62520797983d8e97890
tim2504 <timgulufa@gmail.com>
2024-05-13 19:13:15 +0500

    w

---
+++ app.js
@@ -0,0 +1,37 @@
+ // app.js 
+ const express = require('express');
+ const path = require('path');
+ // Импортируем библиотеку
+ const bodyParser = require('body-parser');
+ const mainRoute = require('./routes/main');
+ const gamesRouter = require('./routes/games'); 
+ 
+ const PORT = 3000;
+ const app = express();
+ 
+ // Настраиваем миддлвар-функцию bodyParser
+ app.use(
+     bodyParser.json(),
+     express.static(path.join(__dirname, 'public')),
+     mainRoute,
+     gamesRouter
+ ); 
+ 
+ app.listen(PORT, () => {
+     console.log(`Server is running at PORT http://localhost:${PORT}`);
+ })
+ 
+
+
+ const sendAllGames = (req, res) => {
+    res.send(req.games);
+  };
+  
+  const sendUpdatedGames = (req, res) => {
+    res.send({
+      games: req.games,
+      updated: req.updatedObject
+    });
+  };
+  
+  module.exports = { sendAllGames, sendUpdatedGames };
---
+++ middlewares/cors.js
@@ -0,0 +1,14 @@
+const allowedCors = [
+    'https://prakticum.yandex.ru',
+    'https://my-website.com',
+    'localhost:3000'
+]
+
+function cors(req, res, next) {
+    const { origin } = req.headers;
+    if(allowedCors.includes(origin)) {
+        res.headers('Access-Control-Allow-Origin', origin);
+    }
+    next();
+}
+module.exports = cors;
